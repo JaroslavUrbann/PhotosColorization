b2e932c9a980f2849ec1a8b89474a3675c808b2f
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-10-29 21:12:53 +0100

    Minor changes

--- main/GoogleColab/main.py
+++ main/GoogleColab/main.py
@@ -7,14 +7,24 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 
 n_images = 1
 batch_size = 1
-images_path = os.path.join(current_directory, "b_probs.zip")
-trained_model_path = os.path.join(current_directory, "pspnet50_ade20k")
+n_epochs = 1
+batches_per_epoch = int(n_images / batch_size)
+validation_batches = 1
+
+images_path = os.path.join(current_directory, "test_dataset.zip")
+trained_model_path = os.path.join(current_directory, "pspnet.h5")
 trained_model = load_trained_model(trained_model_path)
 
-training_data_fn = generator_fn(n_images, batch_size, images_path, trained_model)
+# training_data_fn = generator_fn(n_images, batch_size, images_path, trained_model)
 validation_data_fn = generator_fn(n_images, batch_size, images_path, trained_model)
 model = model_definition()
-train_model(model, training_data_fn, validation_data_fn, 1, 1, 1, current_directory)
+# train_model(model,
+#             training_data_fn,
+#             validation_data_fn,
+#             n_epochs,
+#             batches_per_epoch,
+#             validation_batches,
+#             current_directory)
 
 validate_prediction = False
 n_batches = 1
