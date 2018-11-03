ad9bf69c7c4a3c169f5faa9e0c8e89a100ae802b
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-11-03 19:42:02 +0100

    added "restructure dataset" function for places2 dataset

--- main/clean_dataset.py
+++ main/clean_dataset.py
@@ -2,9 +2,11 @@ from PIL import Image, ImageChops
 import time
 import os
 import shutil
+import random
+import sys
 
-path = "F://coco2017_1/"
-move_to = "F://coco2017_2/"
+path = "F://places2_24Gb_restructured"
+move_to = "F://places2_training_1"
 
 
 def is_grayscale(img_path):
@@ -21,23 +23,41 @@ def is_grayscale(img_path):
 def remove_grayscale(folder_path):
     n_removed_imgs = 0
     image_paths = os.listdir(folder_path)
-    print(len(image_paths))
+    n_images = len(image_paths)
+    print(n_images)
     for i in range(len(image_paths)):
-        if is_grayscale(folder_path + image_paths[i]):
-            os.remove(folder_path + image_paths[i])
+        if is_grayscale(os.path.join(folder_path, image_paths[i])):
+            os.remove(os.path.join(folder_path, image_paths[i]))
             n_removed_imgs += 1
-            print(str(n_removed_imgs) + " | " + str(i) + " / " + str(len(image_paths)))
+            print(str(n_removed_imgs) + " | " + str(100 * i / n_images) + " %")
 
 
-def split_folder(folder_path, move_to_path):
+def split_folder(folder_path, move_to_path, n_parts):
     image_paths = os.listdir(folder_path)
+    random.shuffle(image_paths)
     print(len(image_paths))
-    n_files_to_move = int(len(image_paths) / 2)
+    n_files_to_move = int(len(image_paths) / n_parts)
     for i in range(n_files_to_move):
-        shutil.move(folder_path + image_paths[i], move_to_path + image_paths[i])
-        print(str(i) + " / " + str(n_files_to_move))
+        shutil.move(os.path.join(folder_path, image_paths[i]), os.path.join(move_to_path, image_paths[i]))
+        print(str(100 * i / n_files_to_move) + " %")
+
+
+def restructure_dataset(folder_path, destination_path):
+    subdirectories = [x for x in os.walk(folder_path)]
+    print(len(subdirectories))
+    counter = 1
+    for x in subdirectories:
+        if not x[1]:
+            images = os.listdir(x[0])
+            print(x[0])
+            for i in images:
+                os.rename(os.path.join(x[0], i), os.path.join(destination_path, str(counter) + ".jpg"))
+                counter += 1
+    print(counter)
 
 
 start_time = time.time()
-remove_grayscale(path)
+# remove_grayscale(path)
+split_folder(path, move_to, 26)
+# remove_grayscale(move_to)
 print(str(time.time() - start_time))
