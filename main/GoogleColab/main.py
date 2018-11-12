580ba821f5c2c2fad6b19408d0c13aacc6874d9c
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-11-12 23:09:10 +0100

    removed training options

--- main/GoogleColab/main.py
+++ main/GoogleColab/main.py
@@ -3,32 +3,17 @@ from generator_fn import generator_fn
 from evaluate_model import validate_images
 from train_model import model_definition, train_model, load_trained_model
 from keras.models import load_model
+
+batch_size = 7
+images_size = (256, 256)
+validate_prediction = True
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-batch_size = 1
-batches_per_epoch = 1
-n_epochs = 1
-batches_per_validation = 1
-images_size = (176, 216)
-
-training_path = os.path.join(current_directory, "dataset_training.zip")
-validation_path = os.path.join(current_directory, "test_dataset.zip")
+images_path = os.path.join(current_directory, "places_dataset.zip")
+images_destination = os.path.join(current_directory, "validation")
 trained_model_path = os.path.join(current_directory, "pspnet.h5")
 trained_model = load_trained_model(trained_model_path)
+validation_data_fn = generator_fn(batch_size, images_path, images_size, trained_model, validation=True)
+model = load_model("model_final.hdf5")
 
-# training_data_fn = generator_fn(batch_size, training_path, images_size, trained_model)
-validation_data_fn = generator_fn(batch_size, validation_path, images_size, trained_model)
-# model = model_definition()
-model = load_model("model_checkpoint.hdf5")
-
-# train_model(model,
-#             training_data_fn,
-#             validation_data_fn,
-#             n_epochs,
-#             batches_per_epoch,
-#             batches_per_validation,
-#             current_directory)
-
-validate_prediction = True
-n_batches = 1
-validate_images(validate_prediction, model, validation_data_fn, n_batches, current_directory)
+validate_images(validate_prediction, model, validation_data_fn, images_destination)
