77a676a5b03e6bd4ea0514d4215b7f56371eef13
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-11-19 22:40:04 +0100

    learning kivy

--- main/DesktopApp/main.py
+++ main/DesktopApp/main.py
@@ -5,6 +5,7 @@ from kivy.core.window import Window
 class PhotosColorizationApp(App):
     def build(self):
         Window.bind(on_dropfile=self.on_drop_file)
+        self.Backend = Backend()
         return
 
     def tick_box(self, value):
@@ -12,7 +13,13 @@ class PhotosColorizationApp(App):
 
     def on_drop_file(self, window, file_path):
         print(file_path)
+        self.Backend.get_images(file_path)
         return
+
+
+class Backend:
+    def get_images(self, path):
+        return path
 
 
 PhotosColorizationApp().run()
