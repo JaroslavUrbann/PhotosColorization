33d053a5078600c7a24597027a923779e60069cd
Jaroslav Urban <jaroslav.urbann@gmail.com>
2018-12-10 23:16:47 +0100

    added the dialog for reference

--- main/GoogleColab/reconnect.js
+++ main/GoogleColab/reconnect.js
@@ -29,3 +29,14 @@ async function main() {
 }
 
 main();
+/*
+<colab-dialog class="yes-no-dialog"><colab-dialog-impl role="dialog" tabindex="-1" class="x-scope colab-dialog-impl-0" style="outline: none; position: fixed; top: 308.886px; left: 0.227295px; box-sizing: border-box; max-height: 805.009px; max-width: 1125px;">
+
+    <div class="content-area"><h2>GPU memory usage is close to the limit</h2><div class="flex">Your GPU is close to its memory limit. You will not be able to use any additional memory in this session. Currently, 9.18 GB / 11.17 GB is being used. Would you like to terminate some sessions in order to free up GPU memory (state will be lost for those sessions)?</div></div><div class="buttons"><paper-button id="cancel" dialog-dismiss="" role="button" tabindex="0" animated="" aria-disabled="false" elevation="0" class="x-scope paper-button-0">Ignore</paper-button><paper-button id="ok" autofocus="" dialog-confirm="" role="button" tabindex="0" animated="" aria-disabled="false" elevation="0" class="x-scope paper-button-0">Manage sessions<paper-ripple class="style-scope paper-button">
+
+
+    <div id="background" class="style-scope paper-ripple"></div>
+    <div id="waves" class="style-scope paper-ripple"></div>
+  </paper-ripple></paper-button></div>
+  </colab-dialog-impl></colab-dialog>
+*/
